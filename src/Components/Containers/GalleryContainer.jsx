b66be3887c109465fdbe9d7bd5b3e8dddf8a3602
StephanCodes <stephan_bakker@live.nl>
2022-03-09 16:39:50 +0100

    feat: Added containers for gallery and detail pages

---
+++ src/Components/Containers/GalleryContainer.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+// This container will show the gallery page, showcasing the available items to inspect
+function GalleryContainer() {
+    return (
+        <div>Gallery</div>
+    );
+}
+
+export default GalleryContainer;
