7e3add4e4963ccbde1a6fef91c97b59703eef45e
StephanCodes <stephan_bakker@live.nl>
2022-03-10 15:26:33 +0100

    feat: Set up styling and classes for card component

--- src/Components/Atoms/Card/index.jsx
+++ src/Components/Atoms/Card/index.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import './Card.css';
 
 const GalleryCard = () => {
+    const handleClick = () => {
+
+    };
+
     return (
         <button className="card">
-            Click me
+            <div className="card__image">
+                <img src="https://picsum.photos/300/200" alt="vehicle" />
+            </div>
+            <div className="card__content">
+                <div className="card__title">
+                    [Vehicle name]
+                </div>
+                <div className="card__subtitle">
+                    [Vehicle model]
+                </div>
+            </div>
         </button>
     );
 };
