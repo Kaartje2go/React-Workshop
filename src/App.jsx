51d1993bac89f45648706b45b3e69c7dd7342a21
StephanCodes <stephan_bakker@live.nl>
2022-03-10 16:07:40 +0100

    chore: Added hint for routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
+import { Routes, Route } from 'react-router';
 import GalleryContainer from './Components/Containers/GalleryContainer';
 import './App.css';
-import { Routes, Route } from 'react-router';
 
 const App = () => {
   return (
     <div className="app">
       <Routes>
         <Route path="/" element={<GalleryContainer />} />
+        {/* Here we'll want to add a route to see the details page. Preferably, we use the 'id' of the vehicle in the route. */}
       </Routes>
     </div>
   );
