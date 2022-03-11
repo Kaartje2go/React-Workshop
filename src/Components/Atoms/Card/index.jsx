963240d56e5d58252b90c9d8d977b1debbf7e144
StephanCodes <stephan_bakker@live.nl>
2022-03-11 10:04:52 +0100

    feat: Moved some logic to a helper function.

--- src/Components/Atoms/Card/index.jsx
+++ src/Components/Atoms/Card/index.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import getRandomImageUrl from '../../../Helpers/getRandomImageUrl';
 import './Card.css';
 
 const Card = ({ title }) => {
     // If properties are passed from the parent, we can use them here
-    const imageSeed = title || 'placeholder';
+    const imageUrl = getRandomImageUrl({ seed: title });
 
     const handleClick = () => {
         // To keep the Card component generic, we will pass the click event to the parent.
@@ -12,7 +13,7 @@ const Card = ({ title }) => {
     return (
         <button className="card">
             <div className="card__image">
-                <img src={ `https://picsum.photos/seed/${imageSeed}/300/200` } alt={ title || 'planet' } />
+                <img src={ imageUrl } alt={ title || 'planet' } />
             </div>
             <div className="card__content">
                 <div className="card__title">
