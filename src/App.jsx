6b6576bb852696ed3f9966221c0d07d8665b187f
StephanCodes <stephan_bakker@live.nl>
2022-03-14 15:26:51 +0100

    feat: Added the demo content to the example branch

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from 'react-router';
+import DemoContainer from './Components/Containers/DemoContainer';
 import DetailContainer from './Components/Containers/DetailContainer';
 import GalleryContainer from './Components/Containers/GalleryContainer';
 import './App.css';
@@ -7,8 +8,9 @@ const App = () => {
   return (
     <div className="app">
       <Routes>
-        <Route path="/" element={<GalleryContainer />} />
-        <Route path="/detail/:planetId" element={<DetailContainer />} />
+        <Route path="/" element={ <GalleryContainer /> } />
+        <Route path="/detail/:planetId" element={ <DetailContainer /> } />
+        <Route path="/demo" element={ <DemoContainer /> } />
       </Routes>
     </div>
   );
