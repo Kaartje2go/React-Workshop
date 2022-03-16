f24a05f1597a7b1abb272cd2863fa02258bf836f
Stephan Bakker <58417923+StephanCodes@users.noreply.github.com>
2022-03-16 12:01:47 +0100

    feat: Add demo for practice (#11)

---
+++ src/Components/Containers/DemoContainer/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import DemoComponent from '../../Atoms/DemoComponent';
+
+const DemoContainer = () => {
+    return (
+        <div className="container container--centered">
+            <DemoComponent />
+        </div>
+    );
+}
+
+export default DemoContainer;
