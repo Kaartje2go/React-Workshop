3bb0fc5c85be4d87533872e9c28844c0b96ef672
StephanCodes <stephan_bakker@live.nl>
2022-03-16 21:05:47 +0100

    fix: Emptied Card component for workshop

--- src/Components/Atoms/Card/index.jsx
+++ src/Components/Atoms/Card/index.jsx
@@ -2,29 +2,3 @@ import React from 'react';
 import getRandomImageUrl from '../../../Helpers/getRandomImageUrl';
 import './Card.css';
 
-const Card = ({ title }) => {
-    // If properties are passed from the parent, we can use them here
-    const imageUrl = getRandomImageUrl({ seed: title });
-
-    const handleClick = () => {
-        // To keep the Card component generic, we will pass the click event to the parent.
-    };
-
-    return (
-        <button className="card">
-            <div className="card__image">
-                <img src={ imageUrl } alt={ title || 'planet' } />
-            </div>
-            <div className="card__content">
-                <div className="card__title">
-                    { title || '[Planet name]' } 
-                </div>
-                <div className="card__subtitle">
-                    [Planet climate]
-                </div>
-            </div>
-        </button>
-    );
-};
-
-export default Card;
