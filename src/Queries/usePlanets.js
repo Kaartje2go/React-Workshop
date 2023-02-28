3b9cfd0b125cf16c3f5cfb05cb05d40fb60eb5a7
Stephan Bakker <58417923+StephanCodes@users.noreply.github.com>
2023-02-28 09:34:25 +0100

    chore: Remove id logic (#14)
    * chore: Logic to add id
    
    * chore: Removed outdated comments

--- src/Queries/usePlanets.js
+++ src/Queries/usePlanets.js
@@ -2,17 +2,6 @@ import { useQuery } from 'react-query';
 
 function usePlanets() {
     // We'll get all the planets from the API here, using this URL: https://swapi.dev/api/planets
-    
-    /**
-     * SWAPI does not provide resources with an 'id'.
-     * We can make things a bit easier for ourselves by adding it to every resource with this function.
-     * It uses the 'url' property of a resource to determine the 'id'.
-     * @param {string} resourceUrl 
-     * @returns {string}
-     */
-    const findIdByResourceUrl = (resourceUrl) => {
-        return resourceUrl.match(/\d+/)[0];
-    }
 
     // To retrieve the data with React Query, we'll be using the 'useQuery' hook. You can find the basics here: https://react-query.tanstack.com/guides/queries 
     // To handle the http call itself, we'll use the Fetch API that JS provides: https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
